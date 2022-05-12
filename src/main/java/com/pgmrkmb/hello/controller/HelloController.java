bbe3cc47a947e3b6a5f79e4ae08f25216525bd8a
PgmrKMB <emdk789@naver.com>
2022-05-12 11:36:21 +0900

    first

---
+++ src/main/java/com/pgmrkmb/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.pgmrkmb.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HelloController {
+    
+    @GetMapping(value="/")
+    public String getMethodName() {
+        return "hello";
+    }
+    
+
+}
